6a33c48d16f19a2dbf463f5857bba1e61b88a921
marcprice19 <marceprice19@gmail.com>
2014-12-10 15:31:50 -0700

    add store controller, modified stylesheets, set home page

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ActiveRecord::Base
 validates :title, :description, :image_url, presence: true
 validates :price, numericality: {greater_than_or_equal_to: 0.01}
 validates :title, uniqueness: true
+validates :description, length: {minimum: 10, message: "C'mon, lets be a little more descriptive"}
+
 
 validates :image_url, allow_blank: true, format: { 
   with: %r{\.(gif|jpg|png)\Z}i,
